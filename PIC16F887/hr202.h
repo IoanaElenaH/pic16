0c6b46f7c3e1f39ca8b9737c395d9c5063c1a42f
Shayloh <85832851+Shayloh@users.noreply.github.com>
2022-09-29 14:24:27 +0300

    Update hr202.h

--- PIC16F887/hr202.h
+++ PIC16F887/hr202.h
@@ -1,9 +1,5 @@
-
-
 #ifndef LCD_H
 #define	LCD_H
-
-
 void init_LCD(void);
 void lcd_write(unsigned char c);
 void lcd_clear(void);
@@ -11,10 +7,4 @@ void lcd_goto(unsigned char pos);
 void lcd_puts(char* s);
 void lcd_putch(char c);
 void delay_LCD(unsigned long t);
-
-
-
 #endif
-
-
-
