3f419f181c466ddbd69eda355381587ee62f929a
Shayloh <85832851+Shayloh@users.noreply.github.com>
2022-09-29 14:30:17 +0300

    Update main.c

--- PIC16F887/main.c
+++ PIC16F887/main.c
@@ -2,13 +2,10 @@
 #include "adc.h"
 #include "dht.h"
 #include  "lcd.h"
-
 __PROG_CONFIG(1,0x20D4); 	// config. uC 
 __PROG_CONFIG(2,0x0000); 	// config. uC 
-
 #define LED1 RB0 
 #define LED2 RB1
-
 void init_uC(void); 
 int time_of_day;
 int analogtemp;
@@ -28,35 +25,22 @@ void main(void)
      lcd_putch(analogtemp%10 + '0');
      lcd_putch(223);
 	 lcd_putch('C');
-          
 	 DHT11_read();
-	   
-	 time_of_day=ADC_read(0); // facem conversia ADC si atribuirea o dam variabilei temperatura
-        
-     if(time_of_day<300)
-     {
+	 time_of_day=ADC_read(0); // facem conversia ADC si atribuirea o dam variabilei temperatura  
+    	 if(time_of_day<300){
 		lcd_goto(0x94);
 		lcd_puts("TOD = DAY  ");
-     }
-     else
-     {
+     	}
+    	 else{
 		lcd_goto(0x94);
 		lcd_puts("TOD = NIGHT ");
 	 }
-	
-
-	}
-      
+     }
  }
  
- void init_uC (void) 
-{ 
+ void init_uC (void) { 
 	OSCCON 	= 0x71;			// setez Osc. intern uC de 8MHz // pag. 64
 	TRISB 	= 0b00000000;	// tot Portul B este de iesire
 	TRISD 	= 0b00000000;	// tot Portul B este de iesire
 	TRISA = 0xFF;
-
 } 
-
-
-
