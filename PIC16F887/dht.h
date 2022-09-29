c8161f027c64962a6a6e3cd14c8d9c00d406cfa3
Shayloh <85832851+Shayloh@users.noreply.github.com>
2022-09-29 14:31:05 +0300

    Update dht.h

--- PIC16F887/dht.h
+++ PIC16F887/dht.h
@@ -1,5 +1,4 @@
 #define DHT11_PIN RB0
-
 typedef struct{
     char Humidity_integer;
     char Humidity_decimal;
@@ -7,7 +6,6 @@ typedef struct{
     char Temperature_decimal;
     char Check_sum;
 }DHT11_type;
-
 void DHT11_request(void);
 void DHT11_response(void);
 char DHT11_receive(void);
